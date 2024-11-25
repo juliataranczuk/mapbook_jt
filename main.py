8ae6df92530ee93141cff592557c2e3ceb99ef0f
juliataranczuk <juliataranczuk@gmail.com>
2024-11-25 13:23:38 +0100

    chore: dataset update

---
+++ main.py
@@ -0,0 +1,15 @@
+users:list=[
+    {'name':'Julia', 'posts':1, 'city':'Zamość'},
+    {'name':'Dominik', 'posts':3, 'city':'Poznań'},
+    {'name':'Szymon z wąsem', 'posts':5, 'city':'Toruń'},
+    {'name':'Szymon', 'posts':7, 'city':'Łódź'},
+    {'name':'Patryk', 'posts':9, 'city':'Kielce'},
+
+]
+
+print(f'Witaj {users[0]["name"]}!!')
+for user in users[1:]:
+    print(f'Twój znajomy {user['name']}, z miasta {user['city']}, opublikował {user['posts']}  postów')
+
+# for idx,_ in enumerate(users[1:]):
+#     print(f'Twój znajomy {users[idx]}, opublikował {postow[idx]} postów')
