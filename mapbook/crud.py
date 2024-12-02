a8a8bdc2d7128d15ee9ea28a1b53b8285c3fb989
juliataranczuk <juliataranczuk@gmail.com>
2024-12-02 12:58:15 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -5,3 +5,11 @@ def hello(user:str):
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['name']}, z miejscowości {user['city']} opublikował {user['posts']} postów')
+
+
+def add_user(userlist:list)->None:
+    new_name:str=input('proszę podać imię nowego znajomego')
+    new_posts:int=int(input('podaj liczbę postów'))
+    new_city:str=input('podaj miasto')
+    new_user:dict={'name':new_name, 'posts':new_posts, 'city':new_city}
+    userlist.append(new_user)
