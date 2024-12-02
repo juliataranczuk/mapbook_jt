e801ec5735840340b809e4c454e507d1b4a9db37
juliataranczuk <juliataranczuk@gmail.com>
2024-12-02 14:33:47 +0100

    feat: add remove user function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from mapbook.users import users
-from mapbook.crud import hello,read_users
+from mapbook.crud import hello,read_users, remove_user
+
 
 def main():
     hello(users[0]['name'])
     read_users(users)
-
+    remove_user(users)
+    read_users(users)
 
 if __name__ == '__main__':
     main()
--- mapbook/crud.py
+++ mapbook/crud.py
@@ -7,3 +7,11 @@ def hello(user:str):
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['name']}, z miejscowości {user['city']} opublikował {user['posts']} postów')
+
+
+def remove_user(userlist:list)->None:
+    user_to_find: str = input('podaj imie do usunięcia:')
+    for user in userlist:
+        if user['name'] == user_to_find:
+            print(f'usuwam:{user}')
+            userlist.remove(user)
